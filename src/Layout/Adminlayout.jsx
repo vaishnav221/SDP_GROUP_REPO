f09e1279f2b5028ecc07233dbabb8d2667d12339
Vishmitha-Kumar <125125674+Vishmitha-Kumar@users.noreply.github.com>
2024-07-28 19:57:41 +0530

    Update Adminlayout.jsx

--- src/Layout/Adminlayout.jsx
+++ src/Layout/Adminlayout.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Leftbar from '@/components/Admin/Leftbar'
+import Topbar from '@/components/Admin/Topbar'
 
-const Adminlayout = () => {
+const AdminLayout = () => {
   return (
-    <div>Adminlayout</div>
+    <div className='h-screen w-screen flex items-center'>
+      <div className='fixed h-full w-1/6'>
+        <Leftbar />
+      </div>
+      <div className='w-5/6 ml-auto flex flex-col'>
+        <div className='fixed w-5/6'>
+          <Topbar />
+        </div>
+        <div className='h-[calc(100vh-7vh)] w-full mt-[7vh] overflow-y-auto'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Adminlayout
+export default AdminLayout
