da946d13b7bca77de6fb6bee8fe25a3dc87f7f56
VAISHNAVI M <vaishnavim1194@gmail.com>
2024-08-05 22:19:45 +0530

    halls

--- Hall Booking Management - Frontend/src/components/empty-hotels-state/EmptyHotelsState.jsx
+++ Hall Booking Management - Frontend/src/components/empty-hotels-state/EmptyHotelsState.jsx
@@ -14,9 +14,9 @@ const EmptyHotelsState = () => (
         d="M9.172 20c.818 0 1.544-.312 2.121-.88l6.343-6.344a3 3 0 10-4.243-4.243L7.05 14.88a3 3 0 000 4.243 2.99 2.99 0 002.121.877v0zM13 7h7m0 0v7m0-7l-8 8"
       />
     </svg>
-    <h3 className="text-lg text-gray-800 font-semibold">No Hotels Found</h3>
+    <h3 className="text-lg text-gray-800 font-semibold">No Halls Found</h3>
     <p className="text-gray-500">
-      We can't seem to find any hotels that match your search criteria.
+      We can't seem to find any halls that match your search criteria.
     </p>
   </div>
 );
